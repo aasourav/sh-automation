b94d7225ded7d429ca8d6cb326fa9d8b5947fa62
Ahsan Amin <aasourav@yahoo.com>
2024-03-15 17:18:36 +0600

    add kind

--- minikube-kubectl-kubebuilder-docker-makefile-installation/install.sh
+++ minikube-kubectl-kubebuilder-docker-makefile-installation/install.sh
@@ -38,9 +38,16 @@ go version
 
 #minikube
 #==========================
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start --driver=docker
+# curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# minikube start --driver=docker
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 
 
 #install make
